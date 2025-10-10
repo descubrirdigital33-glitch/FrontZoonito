720838d91f627712b08a1144638ac35fb8a7ce97
descubrirdigital33-glitch <descubrirdigital33@gmail.com>
2025-10-10 13:32:06 -0300

    Update Footer.js

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,7 +1,8 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white p-4 text-center">
-      &copy; {new Date().getFullYear()} MusicApp. Todos los derechos reservados.
+      &copy; {new Date().getFullYear()} MusicApp. Todos los derechos reservados lee mas.
     </footer>
   );
 }
+
