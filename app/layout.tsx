d101fa0e6a7b83943404d21cb65ffcb44aa8765e
descubrirdigital33-glitch <descubrirdigital33@gmail.com>
2025-10-14 08:16:19 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,3 @@
-// import './globals.css';
-// import { ReactNode } from 'react';
-// import { UserProvider } from './context/UserContext';
-// import { ReproductorProvider } from './context/ReproductorContext';
-// import ClientLayout from './components/ClientLayout';
-
-// export const metadata = {
-//   title: 'Lemon Music',
-//   description: 'Music App',
-// };
-
-// interface RootLayoutProps {
-//   children: ReactNode;
-// }
-
-// export default function RootLayout({ children }: RootLayoutProps) {
-//   return (
-//     <html lang="es">
-//       <body>
-//         <UserProvider>
-//           <ReproductorProvider>
-//             <ClientLayout>{children}</ClientLayout>
-//           </ReproductorProvider>
-//         </UserProvider>
-//       </body>
-//     </html>
-//   );
-// }
-
-
-
-
 // app/layout.tsx
 import './globals.css';
 import { ReactNode } from 'react';
